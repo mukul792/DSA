56507ed26d8c4e5e1f29d92db2c970dc8d0cfde2
mukul gupta <99530780+mukul792@users.noreply.github.com>
2025-05-03 02:44:30 +0530

    Time: 16 ms (23.51%), Space: 45 MB (11.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public static HashMap<Character,Integer> mp(String str){
-        HashMap<Character,Integer> check = new HashMap<>();
+        HashMap<Character,Integer> check = new HashMap<>(); //putting both the strings and making frequency map of each character
         for(var e : str.toCharArray()){
             if(check.containsKey(e)){
                 check.put(e,check.get(e)+1);
@@ -12,9 +12,9 @@ class Solution {
         return check;
     }
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){return false;}
+        if(s.length()!=t.length()){return false;} //if length are different then no anagram
         HashMap<Character,Integer> mp1 = mp(s);
         HashMap<Character,Integer> mp2 = mp(t);
-        return mp1.equals(mp2);
+        return mp1.equals(mp2); //checking both the string hashmaps if they are equal true else false
     }
 }
