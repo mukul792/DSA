da3cc71316e66774a9fe680f66684a6b7fe1ee29
mukul gupta <99530780+mukul792@users.noreply.github.com>
2025-05-04 02:46:04 +0530

    Time: 10 ms (76.35%), Space: 42.4 MB (71.26%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,14 +2,15 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         HashMap<Character,Character> mp = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
-            Character sCh = s.charAt(i);
-            Character tCh = t.charAt(i);
+            Character sCh = s.charAt(i);       //0-f , 1-o , 2-o , 3-x
+            Character tCh = t.charAt(i);       //0-b , 1-a , 2-a , 3-a
 
             if(mp.containsKey(sCh)){
-                if(mp.get(sCh)!=tCh) return false;
+                if(mp.get(sCh)!=tCh)            //checks for already existing value of s key must be equal t string value
+                return false;                   //if 2nd time occuring s is not equal to 2nd time occuring t then false
             }
-            else if(mp.containsValue(tCh)){
-                if(mp.get(tCh)!=sCh) return false;
+            else if(mp.containsValue(tCh)){     //checks for the duplication of value 
+                return false;                   //if two keys map same value error
             }
             else{
                 mp.put(sCh,tCh);
